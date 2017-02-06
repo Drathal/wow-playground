c32756b4a3b8813e74d282868216ef6fa798a247
Markus Dethlefsen <markus.dethlefsen@stylelounge.de>
2017-02-07 00:09:20 +0100

    refactor

--- src/addon.ts
+++ src/addon.ts
@@ -8,17 +8,19 @@ export interface IAddon {
 
 export type IAddonList = IAddon[];
 
+export const ADDONS_FILENAME = "AddOns.txt";
+
 export const getPath = (base: string, account: string, server: string, char: string): string => {
-    return path.join(__dirname, base, `${account}/${server}/${char}/AddOns.txt`);
+    return path.join(__dirname, base, `${account}/${server}/${char}/${ADDONS_FILENAME}`);
 };
 
 export const getList = (path: string): IAddonList => {
     return fs.readFileSync(path, "UTF-8", )
         .split("\n")
-        .map(line => line.replace("\r", ""))
-        .map(line => line.split(": "))
-        .map(a => ({
-            addon: a[0],
-            enabled: a[1] === "enabled"
+        .map((line: string) => line.replace("\r", ""))
+        .map((line: string) => line.split(": "))
+        .map((line: Object) => ({
+            addon: line[0],
+            enabled: line[1] === "enabled"
         }));
 };
