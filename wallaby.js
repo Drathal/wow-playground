b1f223794a13a2eddbd9f2316a8455596facdc3b
Markus Dethlefsen <markus.dethlefsen@stylelounge.de>
2017-02-07 11:21:03 +0100

    update config

--- wallaby.js
+++ wallaby.js
@@ -1,20 +1,20 @@
-module.exports = function (wallaby) {
-  return {
-    files: [
-      'test/**/*.txt',
-      'src/**/*.ts',
-      '!src/**/*.test.ts'
-    ],
-    tests: [
-      'src/**/*.test.ts'
-    ],
-    env: {
-      type: 'node'
-    },
-    compilers: {
-     '**/*.ts': wallaby.compilers.typeScript(),
-    },
-    testFramework: 'ava',
-    debug: true
-  };
+module.exports = function(wallaby) {
+    return {
+        files: [
+            'test/**/*',
+            'src/**/*.ts',
+            '!src/**/*.test.ts'
+        ],
+        tests: [
+            'src/**/*.test.ts'
+        ],
+        env: {
+            type: 'node'
+        },
+        compilers: {
+            '**/*.ts': wallaby.compilers.typeScript(),
+        },
+        testFramework: 'ava',
+        debug: false
+    };
 };
--- webpack/config.js
+++ webpack/config.js
@@ -1,54 +1,60 @@
 const webpack = require('webpack');
 const path = require('path');
 const fs = require('fs');
-const { CheckerPlugin, TsConfigPathsPlugin } = require('awesome-typescript-loader');
+const {
+    CheckerPlugin,
+    TsConfigPathsPlugin
+} = require('awesome-typescript-loader');
 const SRC_PATH = path.resolve(__dirname, '../src');
 const DIST_PATH = path.resolve(__dirname, '../dist');
 
 let nodeModules = {};
 
 fs.readdirSync('node_modules')
-  .filter((x) => ['.bin'].indexOf(x) === -1)
-  .forEach((mod) =>  nodeModules[mod] = 'commonjs ' + mod);
-  
+    .filter((x) => ['.bin'].indexOf(x) === -1)
+    .forEach((mod) => nodeModules[mod] = 'commonjs ' + mod);
+
 const config = {
-  target: 'node',
-  node: {
-    __filename: true,
-    __dirname: true
-  },
-  devtool: "source-map",
-  entry: {
-    'app': ['./src/index.ts']
-  },
-  output: {
-    path: DIST_PATH,
-    filename: '[name].bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts','.js'],
-    modules: [SRC_PATH, 'node_modules'],
-  },
-  externals: nodeModules,
-  module: {
-    rules: [
-      { test: /\.ts$/, exclude: /node_modules/, loader: 'awesome-typescript-loader' }
+    stats: "errors-only",
+    target: 'node',
+    node: {
+        __filename: true,
+        __dirname: true
+    },
+    devtool: "source-map",
+    entry: {
+        'app': ['./src/index.ts']
+    },
+    output: {
+        path: DIST_PATH,
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        modules: [SRC_PATH, 'node_modules'],
+    },
+    externals: nodeModules,
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            loader: 'awesome-typescript-loader'
+        }]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new CheckerPlugin(),
+        new TsConfigPathsPlugin()
+        /*,
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: (module) => (module.context && module.context.indexOf('node_modules') !== -1)
+        })
+        */
     ]
-  },
-  plugins: [
-       new webpack.LoaderOptionsPlugin({
-         minimize: true,
-         debug: false
-       }),
-       new CheckerPlugin(),
-       new TsConfigPathsPlugin()
-       /*,
-       new webpack.optimize.CommonsChunkPlugin({
-           name: 'vendor',
-           minChunks: (module) => (module.context && module.context.indexOf('node_modules') !== -1)
-       })
-       */
-  ]
 };
 
 module.exports = config;
